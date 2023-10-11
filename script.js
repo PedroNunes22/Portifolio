98fb34e24ee0ce69fb003174d88ad636be564419
Pedro Nunes <pedrofais11@gmail.com>
2023-10-11 13:24:14 -0300

    portifólio

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function toggleMode(){
-const html = document.documentElement
-html.classList.toggle('light')
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-const img = document.querySelector("#profile img")
+  const img = document.querySelector("#profile img")
 
-if(html.classList.contains('light')){
-  img.setAttribute('src', './assets/avatar-light.png')
-} else {
-  img.setAttribute("src", "./assets/avatar.png")
-}
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.jpeg")
+  } else {
+    img.setAttribute("src", "./assets/avatar.jpeg")
+  }
 }
